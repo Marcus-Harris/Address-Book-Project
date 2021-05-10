058e6ea32376716308ecfd8d8f3287b7ddd3d1ba
Marcus Harris <79154965+Marcus-Harris@users.noreply.github.com>
2021-05-10 16:29:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,56 +3,42 @@ package com.tts;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        AddressBook addressFunction = new AddressBook();
 
-        System.out.println("Please choose what you'd like to do with the database: ");
-        int userDecision = scanner.nextInt();
+        while (true) {
 
-        int i = 1;
-        int myId = i;
+            System.out.println("Please choose what you'd like to do with the address book: ");
+            Integer userDecision = scanner.nextInt();
 
+            switch (userDecision) {
+                case 1:
+                    addressFunction.add();
+                    break;
 
+                case 2:
+                    addressFunction.deletePerson();
+                    break;
 
-        switch (userDecision) {
-            case 1:
-                System.out.println("Enter your first name.");
-                String firstName = scanner.next();
+                case 3:
+                    addressFunction.search();
+                    break;
+                case 4:
+                    addressFunction.print();
+                    break;
+                case 5:
+                    addressFunction.deleteAddressBook();
+                    break;
 
-                System.out.println("Enter your last name.");
-                String lastName = scanner.next();
+                case 6:
+                    addressFunction.quit();
+                    break;
 
-                System.out.println("Enter your phone number.");
-                int phoneNumber = scanner.nextInt();
-
-                System.out.println("Enter your email address.");
-                String emailAddress = scanner.next();
-
-                new Add(myId, firstName, lastName, phoneNumber, emailAddress);
-                System.out.println("Your entry has been added");
-                break;
-
-            case 2:
-                // new Remove();
-                break;
-
-            case 3:
-                // new Search();
-                break;
-            case 4:
-                // new Print();
-                break;
-            case 5:
-                // new DeleteBook();
-                break;
-
-            case 6:
-                // new QuitProgram();
-                break;
-
-            default:
-                System.out.println("Please choose a number between 1 and 6 (inclusive).");
-                main(new String[0]);
+                default:
+                    System.out.println("Please choose a number between 1 and 6 (inclusive).");
+            }
         }
     }
 }
